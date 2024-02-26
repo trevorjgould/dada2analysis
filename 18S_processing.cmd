f257fe8bf92f692ba65d0fdbfbf83872c1c3605c
Trevor Gould <trevorjgould@gmail.com>
2024-02-26 10:55:59 -0600

    Update 18S_processing.cmd
    update 18S

--- 18S_processing.cmd
+++ 18S_processing.cmd
@@ -12,8 +12,8 @@ TGATCCTTCTGCAGGTTCACCTAC
 # remove adapters
 mkdir 01_adapter
 for i in *_R1_001.fastq.gz; do echo "~/.local/bin/cutadapt --cores 4 --minimum-length 150 -a TATGGTAATTGTGTGNCAGCNGCCGCGGTAA -g ATTAGANACCCNNGTAGTCCGGCTGGCTGACT -A AGTCAGCCAGCCGGACTACNVGGGTNTCTAAT -o 01_adapter/${i} -p 01_adapter/${i//_R1_/_R2_} ${i} ${i//_R1_/_R2_} > 01_adapter/cutadapt.${i//_R1_001.fastq.gz/.log.txt}" >> run_cutadapt.sh; done
-chmod +x run_cutadapt.sh
-./run_cutadapt.sh
+chmod +x run_cutadapt.cmd
+./run_cutadapt.cmd
 cd 01_adapter    
 mkdir 01_logs
 mv cutadapt* 01_logs
@@ -23,9 +23,9 @@ grep "passing" 01_logs/* > summary_adapter_trimming.txt
 # remove primers
 mkdir ../02_filtered  
 #18S
-#for i in *_R1_001.fastq.gz; do echo "~/.local/bin/cutadapt --cores 4 --minimum-length 100 --discard-untrimmed -g GTACACACCGCCCGTC -G TGATCCTTCTGCAGGTTCACCTAC -a GTAGGTGAACCTGCAGAAGGATCA -A GACGGGCGGTGTGTAC --discard-untrimmed -o ../02_filtered/${i} -p ../02_filtered/${i//_R1_/_R2_} ${i} ${i//_R1_/_R2_} > ../02_filtered/cutadapt.${i//_R1_001.fastq.gz/.adapter.log.txt}" >> run_cutadapt2.cmd; done
+#for i in *_R1_001.fastq.gz; do echo "cutadapt --cores 4 --minimum-length 100 --discard-untrimmed -g GTACACACCGCCCGTC -G TGATCCTTCTGCAGGTTCACCTAC -a GTAGGTGAACCTGCAGAAGGATCA -A GACGGGCGGTGTGTAC --discard-untrimmed -o ../02_filtered/${i} -p ../02_filtered/${i//_R1_/_R2_} ${i} ${i//_R1_/_R2_} > ../02_filtered/cutadapt.${i//_R1_001.fastq.gz/.adapter.log.txt}" >> run_cutadapt2.cmd; done
 #18S WANDA1 AML2
-for i in *_R1_001.fastq.gz; do echo "~/.local/bin/cutadapt --cores 4 --minimum-length 100 --discard-untrimmed -g CAGCCGCGGTAATTCCAGCT -G GAACCCAAACACTTTGGTTTCC -a AGCTGGAATTACCGCGGCTG -A GGAAACCAAAGTGTTTGGGTTC --discard-untrimmed -o ../02_filtered/${i} -p ../02_filtered/${i//_R1_/_R2_} ${i} ${i//_R1_/_R2_} > ../02_filtered/cutadapt.${i//_R1_001.fastq.gz/.adapter.log.txt}" >> run_cutadapt2.cmd; done
+for i in *_R1_001.fastq.gz; do echo "cutadapt --cores 4 --minimum-length 100 --discard-untrimmed -g CAGCCGCGGTAATTCCAGCT -G GAACCCAAACACTTTGGTTTCC -a AGCTGGAATTACCGCGGCTG -A GGAAACCAAAGTGTTTGGGTTC --discard-untrimmed -o ../02_filtered/${i} -p ../02_filtered/${i//_R1_/_R2_} ${i} ${i//_R1_/_R2_} > ../02_filtered/cutadapt.${i//_R1_001.fastq.gz/.adapter.log.txt}" >> run_cutadapt2.cmd; done
 
 
 chmod +x run_cutadapt2.cmd
@@ -99,3 +99,8 @@ saveRDS(taxamaar, file = "18SmaarjamtaxID.rds")
 taxrefb <- "/home/umii/goul0109/pr2_version_5.0.0_SSU_dada2.fasta.gz" 
 taxaPR2 <- assignTaxonomy(seqtab.nochim, taxrefb, multithread=TRUE, minBoot = 95, verbose = TRUE, taxLevels=c("Kingdom", "Supergroup", "Division", "Class", "Order", "Family", "Genus", "Species"))
 saveRDS(taxaPR2, file = "18StaxID.rds")
+
+
+
+for i in *_R1_001.fastq.gz; do echo "cutadapt --cores 4 --minimum-length 150 -a TATGGTAATTGTGTGNCAGCNGCCGCGGTAA -g ATTAGANACCCNNGTAGTCCGGCTGGCTGACT -A AGTCAGCCAGCCGGACTACNVGGGTNTCTAAT -o 01_adapter/${i} -p 01_adapter/${i//_R1_/_R2_} ${i} ${i//_R1_/_R2_} > 01_adapter/cutadapt.${i//_R1_001.fastq.gz/.log.txt}" >> run_cutadapt.sh; done
+chmod +x run_cutadapt.sh

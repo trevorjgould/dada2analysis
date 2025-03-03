ab280ad88879f3de8dd2def92b557dbcda93467d
Trevor Gould <trevorjgould@gmail.com>
2025-03-03 14:54:48 -0600

    Update Taxonomy_Plot_Single.R

--- R/Taxonomy_Plot_Single.R
+++ R/Taxonomy_Plot_Single.R
@@ -22,7 +22,7 @@ Create_Taxonomy_Plot <- function(taxtable,t,metadata){
     xt=xt[(rowSums(xt)>0),]
 
     # fix rownames . to -
-    row.names(metadata) <- gsub("-", "\\.", row.names(metadata))
+    # row.names(metadata) <- gsub("-", "\\.", row.names(metadata))
 
     #find common names
     common <- intersect(rownames(metadata),rownames(xt))
@@ -85,11 +85,12 @@ Create_Taxonomy_Plot <- function(taxtable,t,metadata){
     filename2 <- paste0(name_label,"_taxonomy_top_",t,".txt")
     titlename <- paste0("Top_",t,"_",name_label)
     # colors
+    c25 <- c("dodgerblue2","#E31A1C","green4","#6A3D9A","#FF7F00","black", "gold1","skyblue2", "#FB9A99","palegreen2","#CAB2D6","#FDBF6F","gray70", "khaki2","maroon", "orchid1", "deeppink1", "blue1", "steelblue4","darkturquoise", "green1", "yellow4", "yellow3","darkorange4", "brown")
     AsteroidCityall = c("#0A9F9D", "#CEB175", "#E54E21", "#6C8645", "#C18748","#C52E19", "#AC9765", "#54D8B1", "#b67c3b", "#175149", "#AF4E24", "#FBA72A", "#D3D4D8", "#CB7A5C", "#5785C1")
 	GrandBudapest1 = c("#E6A0C4", "#C6CDF7", "#D8A499", "#7294D4","#F1BB7B", "#FD6467", "#5B1A18", "#D67236")
 	#getPalette = RColorBrewer::colorRampPalette(brewer.pal(t, "Set1"))
 
-	p <- ggplot2::ggplot(data = melted[!is.na(melted$SampleID),], ggplot2::aes(Samples, (value*100), fill = taxonomy)) + ggplot2::geom_bar(stat='identity')+ ggplot2::ylab("Percent") + ggplot2::scale_x_discrete(expand = c(0, 0.5)) + ggplot2::xlab("Samples") + theme_Publication() + ggplot2::scale_fill_manual(values = AsteroidCityall) + ggplot2::guides(fill = ggplot2::guide_legend(ncol = 2,title=name_label))
+	p <- ggplot2::ggplot(data = melted[!is.na(melted$SampleID),], ggplot2::aes(Samples, (value*100), fill = taxonomy)) + ggplot2::geom_bar(stat='identity')+ ggplot2::ylab("Percent") + ggplot2::scale_x_discrete(expand = c(0, 0.5)) + ggplot2::xlab("Samples") + theme_Publication() + ggplot2::scale_fill_manual(values = c25) + ggplot2::guides(fill = ggplot2::guide_legend(ncol = 2,title=name_label))
     ggplot2::ggsave(p, file = filename, dpi  = 800, width = 14, height = 8, units = "in")
     utils::write.table(both, file = filename2, sep = "\t", quote = FALSE)
     return(p)
@@ -107,7 +108,7 @@ Create_Taxonomy_Plot_Facet <- function(taxtable,t,metadata){
     xt=xt[(rowSums(xt)>0),]
     
     # fix rownames . to -
-    row.names(metadata) <- gsub("-", "\\.", row.names(metadata))
+    # row.names(metadata) <- gsub("-", "\\.", row.names(metadata))
     
     #find common names
     common <- intersect(rownames(metadata),rownames(xt))
@@ -164,6 +165,7 @@ Create_Taxonomy_Plot_Facet <- function(taxtable,t,metadata){
     # filenames
 
     # colors
+    c25 <- c("dodgerblue2","#E31A1C","green4","#6A3D9A","#FF7F00","black", "gold1","skyblue2", "#FB9A99","palegreen2","#CAB2D6","#FDBF6F","gray70", "khaki2","maroon", "orchid1", "deeppink1", "blue1", "steelblue4","darkturquoise", "green1", "yellow4", "yellow3","darkorange4", "brown")
     AsteroidCityall = c("#0A9F9D", "#CEB175", "#E54E21", "#6C8645", "#C18748","#C52E19", "#AC9765", "#54D8B1", "#b67c3b", "#175149", "#AF4E24", "#FBA72A", "#D3D4D8", "#CB7A5C", "#5785C1")
     GrandBudapest1 = c("#E6A0C4", "#C6CDF7", "#D8A499", "#7294D4","#F1BB7B", "#FD6467", "#5B1A18", "#D67236")
     #getPalette = RColorBrewer::colorRampPalette(brewer.pal(t, "Set1"))
@@ -171,7 +173,7 @@ facetPlot <- function(i){
 		filename <- paste0(name_label,"_taxonomy_top",t,"_in_",i,"_Type_boxplot.png")
     	filename2 <- paste0(name_label,"_taxonomy_top_",t,"_in_",i,".txt")
    		titlename <- paste0("Top_",t,"_in_",i,"_",name_label)
-        p <- ggplot2::ggplot(data = melted[!is.na(melted$SampleID),], ggplot2::aes(Samples, (value*100), fill = taxonomy)) + ggplot2::geom_bar(stat='identity')+ ggplot2::ylab("Percent") + ggplot2::scale_x_discrete(expand = c(0, 0.5)) + ggplot2::xlab("Samples") + facet_wrap(~get(i), scales = "free") + theme_Publication() + ggplot2::scale_fill_manual(values = AsteroidCityall) + ggplot2::guides(fill = ggplot2::guide_legend(ncol = 2,title=name_label))
+        p <- ggplot2::ggplot(data = melted[!is.na(melted$SampleID),], ggplot2::aes(Samples, (value*100), fill = taxonomy)) + ggplot2::geom_bar(stat='identity')+ ggplot2::ylab("Percent") + ggplot2::scale_x_discrete(expand = c(0, 0.5)) + ggplot2::xlab("Samples") + ggplot2::facet_wrap(~get(i), scales = "free") + theme_Publication() + ggplot2::scale_fill_manual(values = c25) + ggplot2::guides(fill = ggplot2::guide_legend(ncol = 2,title=name_label))
         ggplot2::ggsave(p, file = filename, dpi  = 800, width = 14, height = 8, units = "in")
         utils::write.table(both, file = filename2, sep = "\t", quote = FALSE)
         return(p)
@@ -192,7 +194,7 @@ Create_Taxonomy_Plot_Facet_single <- function(taxtable,t,metadata,var1){
     xt=xt[(rowSums(xt)>0),]
     
     # fix rownames . to -
-    row.names(metadata) <- gsub("-", "\\.", row.names(metadata))
+    # row.names(metadata) <- gsub("-", "\\.", row.names(metadata))
     
     #find common names
     common <- intersect(rownames(metadata),rownames(xt))
@@ -249,6 +251,7 @@ Create_Taxonomy_Plot_Facet_single <- function(taxtable,t,metadata,var1){
     # filenames
 
     # colors
+    c25 <- c("dodgerblue2","#E31A1C","green4","#6A3D9A","#FF7F00","black", "gold1","skyblue2", "#FB9A99","palegreen2","#CAB2D6","#FDBF6F","gray70", "khaki2","maroon", "orchid1", "deeppink1", "blue1", "steelblue4","darkturquoise", "green1", "yellow4", "yellow3","darkorange4", "brown")
     AsteroidCityall = c("#0A9F9D", "#CEB175", "#E54E21", "#6C8645", "#C18748","#C52E19", "#AC9765", "#54D8B1", "#b67c3b", "#175149", "#AF4E24", "#FBA72A", "#D3D4D8", "#CB7A5C", "#5785C1")
     GrandBudapest1 = c("#E6A0C4", "#C6CDF7", "#D8A499", "#7294D4","#F1BB7B", "#FD6467", "#5B1A18", "#D67236")
     #getPalette = RColorBrewer::colorRampPalette(brewer.pal(t, "Set1"))
@@ -256,7 +259,7 @@ facetPlot <- function(i){
 		filename <- paste0(name_label,"_taxonomy_top",t,"_in_",i,"_Type_boxplot.png")
     	filename2 <- paste0(name_label,"_taxonomy_top_",t,"_in_",i,".txt")
    		titlename <- paste0("Top_",t,"_in_",i,"_",name_label)
-        p <- ggplot2::ggplot(data = melted[!is.na(melted$SampleID),], ggplot2::aes(Samples, (value*100), fill = taxonomy)) + ggplot2::geom_bar(stat='identity')+ ggplot2::ylab("Percent") + ggplot2::scale_x_discrete(expand = c(0, 0.5)) + ggplot2::xlab("Samples") + facet_wrap(~get(i), scales = "free") + theme_Publication() + ggplot2::scale_fill_manual(values = AsteroidCityall) + ggplot2::guides(fill = ggplot2::guide_legend(ncol = 2,title=name_label))
+        p <- ggplot2::ggplot(data = melted[!is.na(melted$SampleID),], ggplot2::aes(Samples, (value*100), fill = taxonomy)) + ggplot2::geom_bar(stat='identity')+ ggplot2::ylab("Percent") + ggplot2::scale_x_discrete(expand = c(0, 0.5)) + ggplot2::xlab("Samples") + ggplot2::facet_wrap(~get(i), scales = "free") + theme_Publication() + ggplot2::scale_fill_manual(values = c25) + ggplot2::guides(fill = ggplot2::guide_legend(ncol = 2,title=name_label))
         ggplot2::ggsave(p, file = filename, dpi  = 800, width = 14, height = 8, units = "in")
         utils::write.table(both, file = filename2, sep = "\t", quote = FALSE)
         return(p)
